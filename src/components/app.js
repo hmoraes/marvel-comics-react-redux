303b6d7f0c7ee25e65781a73f8cf4bc6096ffd88
Heberte Fernandes de Moraes <brebete@gmail.com>
2018-04-08 17:17:32 -0300

    Clean App code removing unused features

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,3 @@
-import agent from '../agent';
 import Header from './header';
 import ComicList from './comic-list';
 import {APP_LOAD} from '../constants/action-types';
@@ -21,25 +20,7 @@ const mapDispatchToProps = dispatch => ({
 });
 
 class App extends React.Component {
-	componentWillReceiveProps(nextProps) {
-		// if (nextProps.redirectTo) {
-		// 	// this.context.router.replace(nextProps.redirectTo);
-		// 	store.dispatch(push(nextProps.redirectTo));
-		// 	this.props.onRedirect();
-		// }
-	}
-
 	componentWillMount() {
-		const apikey = process.env.REACT_APP_MARVEL_APIKEY;
-		if (apikey) {
-			agent.setApikey(apikey);
-		}
-
-		const limitCount = process.env.REACT_APP_LIMIT_COUNT;
-		if (limitCount) {
-			agent.setLimitCount(limitCount);
-		}
-
 		this.props.onLoad();
 	}
 
